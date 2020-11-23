1dad00009f708ea91af7940bb956bce87a4ab962
James Sheppard <jgsheppard92@gmail.com>
2020-11-23 17:40:13 +0100

    Update cypress tests

--- cypress/integration/changeUserInfo.spec.js
+++ cypress/integration/changeUserInfo.spec.js
@@ -33,5 +33,24 @@ describe('Create account and login', () => {
     cy.get('[data-cy=edit-user-email]').clear();
     cy.get('[data-cy=edit-user-email]').type('bearmanpig@bear.com');
     cy.get('[data-cy=submit-edits]').click();
+
+    // Edit user info to original form
+    cy.get('[data-cy=edit-user-info]').click();
+    // Edit first name
+    cy.get('[data-cy=edit-user-firstname]').clear();
+    cy.get('[data-cy=edit-user-firstname]').type('John');
+    // Edit last name
+    cy.get('[data-cy=edit-user-lastname]').clear();
+    cy.get('[data-cy=edit-user-lastname]').type('Smith');
+    // Edit username
+    cy.get('[data-cy=edit-user-username]').clear();
+    cy.get('[data-cy=edit-user-username]').type('catdog');
+    //Edit email
+    cy.get('[data-cy=edit-user-email]').clear();
+    cy.get('[data-cy=edit-user-email]').type('Jsmith@smith.com');
+    cy.get('[data-cy=submit-edits]').click();
+    // Logout
+    cy.get('[data-cy=hamburger-menu]').click();
+    cy.get('[data-cy=go-to-logout]').click();
   });
 });
