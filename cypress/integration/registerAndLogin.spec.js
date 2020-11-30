8424cd924ce34c3719faf0736de5c6a7227c295c
James Sheppard <63818299+jgsheppa@users.noreply.github.com>
2020-11-30 15:30:14 +0100

    Add visibility to hamburger menu

--- cypress/integration/registerAndLogin.spec.js
+++ cypress/integration/registerAndLogin.spec.js
@@ -5,7 +5,7 @@ describe('Create account and login', () => {
     cy.visit('localhost:3000');
 
     // Click and go to login
-    cy.get('[data-cy=hamburger-menu]').click();
+    cy.get('[data-cy=hamburger-menu]').should('be.visible').click();
     cy.get('[data-cy=go-to-login]').click();
 
     // Click and go to register
@@ -27,7 +27,7 @@ describe('Create account and login', () => {
     cy.get('[data-cy=register-user]').click();
 
     // Go to login
-    cy.get('[data-cy=hamburger-menu]').click();
+    cy.get('[data-cy=hamburger-menu]').should('be.visible').click();
     cy.get('[data-cy=go-to-login]').click();
     // Enter username
     cy.get('[data-cy=username]').type('catdog');
