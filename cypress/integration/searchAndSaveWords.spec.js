be7dca106dba4c35d7937dc00372edee16a28448
James Sheppard <63818299+jgsheppa@users.noreply.github.com>
2020-12-01 00:37:16 +0100

    Add visibility to cypress functions

--- cypress/integration/searchAndSaveWords.spec.js
+++ cypress/integration/searchAndSaveWords.spec.js
@@ -5,9 +5,8 @@ describe('Search for words and save them to list', () => {
     cy.visit('localhost:3000');
 
     // Click and go to login
-    cy.get('[data-cy=hamburger-menu]').should('be.visible');
-    cy.get('[data-cy=hamburger-menu]').click();
-    cy.get('[data-cy=go-to-login]').click();
+    cy.get('[data-cy=hamburger-menu]').should('be.visible').click();
+    cy.get('[data-cy=go-to-login]').should('be.visible').click();
 
     // Click and go to register
     cy.get('[data-cy=go-to-register]').should('be.visible');
@@ -57,7 +56,7 @@ describe('Search for words and save them to list', () => {
     cy.get('[data-cy=search]').click();
 
     // Add word to list
-    cy.get('[data-cy=click-on-list]').click();
+    cy.get('[data-cy=click-on-list]').should('be.visible').click();
     cy.get('[data-cy=add-to-list]').click({ multiple: true });
     cy.get('[data-cy=close-list-box]').click();
 
