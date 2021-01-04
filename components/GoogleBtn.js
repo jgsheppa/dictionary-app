6f90f2b3ab52b3dcbf74d6a00f61b6887ede2169
James Sheppard <63818299+jgsheppa@users.noreply.github.com>
2021-01-04 15:27:11 +0100

    Add styles to logout button

--- components/GoogleBtn.js
+++ components/GoogleBtn.js
@@ -83,6 +83,28 @@ export default function GoogleBtn() {
           buttonText="Logout"
           onLogoutSuccess={logout}
           onFailure={handleLogoutFailure}
+          render={(renderProps) => (
+            <button
+              onClick={renderProps.onClick}
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'left',
+                padding: '10px 30px',
+                borderRadius: '4px',
+                width: '300px',
+                fontSize: '24px',
+                color: '#666',
+                cursor: 'pointer',
+              }}
+            >
+              <img
+                src="/google_svg.png"
+                style={{ width: '36px', height: '36px', marginRight: '40px' }}
+              ></img>
+              Google Login{' '}
+            </button>
+          )}
         ></GoogleLogout>
       ) : (
         <GoogleLogin
@@ -106,6 +128,7 @@ export default function GoogleBtn() {
                 width: '300px',
                 fontSize: '24px',
                 color: '#666',
+                cursor: 'pointer',
               }}
             >
               <img
