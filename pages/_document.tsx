6d2058ebcbc0fc6c3c5e808ae4ffe425be52aa0e
James Sheppard <63818299+jgsheppa@users.noreply.github.com>
2021-01-05 13:43:24 +0100

    Change Google Tracking var logic

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,23 +1,31 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { GA_TRACKING_ID } from '../util/gtag';
 
+const isDevelopment = process.env.NODE_ENV === 'development';
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
           {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
+          {isDevelopment &&  <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
+          />}}
+           {isProduction &&  <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />}}
+         
           <script
             dangerouslySetInnerHTML={{
               __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${GA_TRACKING_ID}', {
+              gtag('config', '${GA_TRACKING_ID_PRODUCTION}', {
                 page_path: window.location.pathname,
               });
           `,
